01d36151d163888fa59e385d37b945c63c1c4f23
fernando.teles <fernando.teles@tcu.gov.br>
2026-06-22 12:33:46 -0300

    Add initial sitemap.xml for SEO optimization

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { createRoot } from "react-dom/client";
+import { clarity } from "react-microsoft-clarity";
 import App from "./app/App";
 import "./styles/index.css";
+
+clarity.init("xb2tldxn72");
 
 createRoot(document.getElementById("root")!).render(<App />);
